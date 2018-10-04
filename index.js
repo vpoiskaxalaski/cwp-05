96ee68b32d70c723ba52cd1baeeaabb70617dd45
Alena <awff@mail.ru>
2018-10-04 23:23:14 +0300

    add CRUD operations

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const http = require('http');
+const fs = require("fs");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const handlers = {
-  '/sum': sum
+  '/api/articles/readall': readall, //возвращает массив статей с комментариями
+  '/api/articles/read': read,//возвращает статью с комментариями по переданному в теле запроса id 
+  '/api/articles/create': create, //создает статью с переданными в теле запроса параметрами / id генерируется на сервере / сервер возвращает созданную статью 
+  '/api/articles/update': update, //обновляет статью с переданными параметрами по переданному id
+  '/api/articles/delete': deleteArticle //удаляет комментарий по переданному id
 };
-
-const fs = require("fs");
 
 
 
@@ -33,51 +36,90 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  const comments = [
-    {
-        "id": "0",
-        "articleId":"2",
-        "text": "shure",
-        "date": "12.12.2016",
-        "author": "Vitalik"
-    },
-    {
-        "id": "1",
-        "articleId":"1",
-        "text": "Burn you!",
-        "date": "12.02.1796",
-        "author": "Efim"
-    },
-    {
-        "id": "3",
-        "articleId":"2",
-        "text": "wow",
-        "date": "05.06.2009",
-        "author": "Alina"
-    }
-  ]
-
-  const articles = {
-    "id": "1",
-    "title": "siens",
-    "text": "Earth is round!" ,
-    "date": "05.01.1686",
-    "author": "Gaalileo Galiley",
-    "comments": comments
-  }
-
-  const a = JSON.stringify(articles);
-  fs.writeFileSync("E:\\University\\3k1s\\PSKP\\git_tutorial\\work\\cwp-05\\jsn.json", a);
-
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 function getHandler(url) {
   return handlers[url] || notFound;
 }
 
-function sum(req, res, payload, cb) {
-  const result = { c: payload.a + payload.b };
+//удаляет комментарий по переданному id
+function deleteArticle(req, res, payload, cb) {
+
+  let id = payload.id;
+
+  const fileContent = fs.readFileSync("articles.json");
+  const content = JSON.parse(fileContent);
+
+  
+  content[--id] ="{}";
+
+  const newJson = JSON.stringify(content);
+  fs.writeFileSync("js.json", newJson);
+  
+  const result = "Article deleted";
+
+  cb(null, result);
+}
+
+//обновляет статью с переданными параметрами по переданному id
+function update(req, res, payload, cb) {
+
+  let id = payload['id'];
+  console.log(id);
+
+  const fileContent = fs.readFileSync("articles.json");
+  const content = JSON.parse(fileContent);
+
+  content[--id] = payload;
+
+  const newJson = JSON.stringify(content);
+  fs.writeFileSync("js.json", newJson);
+  
+  const result = "Article updated";
+
+  cb(null, result);
+}
+
+//создает статью с переданными в теле запроса параметрами / id генерируется на сервере / сервер возвращает созданную статью 
+function create(req, res, payload, cb) {
+
+  let article = payload;
+  let id = 4;
+
+  article['id'] = id;
+  article['comments'][0]['articleId'] = id;
+                              
+  const result = article;
+
+  cb(null, result);
+}
+
+//возвращает статью с комментариями по переданному в теле запроса id 
+function read(req, res, payload, cb) {
+
+  const fileContent = fs.readFileSync("articles.json");
+  const response = JSON.parse(fileContent);
+
+  let id = payload.id;
+  let message ="Header: "+ response[id]['title']+ "[ " + response[id]['text'] + " ]";
+  message += " Comments: " + response[id]['comments'][0]['text'] + ', ' + response[id]['comments'][1]['text'];
+                              
+  const result = message;
+
+  cb(null, result);
+}
+//возвращает массив статей с комментариями
+function readall(req, res, payload, cb) {
+
+  const fileContent = fs.readFileSync("articles.json");
+  const response = JSON.parse(fileContent);
+  let message = response[0]['title'] + '( '+ response[0]['comments'][0]['text'] + ',' 
+                                            + response[0]['comments'][1]['text'] + ' );\r\n';
+  message += response[1]['title'] + '( '+  response[1]['comments'][0]['text'] + ', '
+                                         + response[1]['comments'][1]['text'] +' ); \r\n';
+                                              
+  const result = message;
 
   cb(null, result);
 }
